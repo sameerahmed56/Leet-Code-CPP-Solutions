a9475972420066f1bd7b6e9cb67a6c13e938d08d
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-01 00:22:14 +0530

    Time: 239 ms (66.85%), Space: 49.7 MB (95.34%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -7,42 +7,24 @@ public:
         int curr = -1;
         for(int i = 0; i < m; i++){
             if(seats[i][0] == curr){
-                if(seats[i][1] >= 2 && seats[i][1] <= 5){
-                    leftBlock = true;
-                }
-                if(seats[i][1] >= 4 && seats[i][1] <= 7){
-                    midBlock = true;
-                }
-                if(seats[i][1] >= 6 && seats[i][1] <= 9){
-                    rightBlock = true;
-                }
+                if(seats[i][1] >= 2 && seats[i][1] <= 5) leftBlock = true;
+                if(seats[i][1] >= 4 && seats[i][1] <= 7) midBlock = true;
+                if(seats[i][1] >= 6 && seats[i][1] <= 9) rightBlock = true;
             }
             else{
                curr = seats[i][0];
-                if(leftBlock){
-                    ans--;
-                    if(rightBlock && midBlock){
-                        ans--;
-                    }
-                }
-                else if(rightBlock){
-                    ans--;
-                }
+                if(leftBlock) --ans;
+                if(rightBlock) --ans;
+                if(leftBlock && rightBlock  && !midBlock) ++ans;
                 i--;
                 leftBlock = false;
                 midBlock = false;
                 rightBlock = false;
             }
         }
-        if(leftBlock){
-            ans--;
-            if(rightBlock && midBlock){
-                ans--;
-            }
-        }
-        else if(rightBlock){
-            ans--;
-        }
+        if(leftBlock) --ans;
+        if(rightBlock) --ans;
+        if(leftBlock && rightBlock  && !midBlock) ++ans;
         return ans;
     }
 };

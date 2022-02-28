0f3c41da1c25e4972a27c1876f0f0c18b731cc24
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-01 00:44:45 +0530

    Time: 3 ms (96.69%), Space: 11.5 MB (33.87%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    vector<string> ans;
-    void permutate(string curr,string s, int i){
+    
+    void permutate(string curr,string s, int i,vector<string> &ans){
         if(i>=s.size()){
             ans.push_back(curr);
             return;
         }
         if(isdigit(s[i])){
             curr.push_back(s[i]);
-            permutate(curr,s,i+1);
+            permutate(curr,s,i+1,ans);
         }
         else{
             string c1 = curr;
             c1.push_back(toupper(s[i]));
-            permutate(c1,s,i+1);
+            permutate(c1,s,i+1,ans);
             string c2 = curr;
             c2.push_back(tolower(s[i]));
-            permutate(c2,s,i+1);
+            permutate(c2,s,i+1,ans);
             
         }
     }
     vector<string> letterCasePermutation(string s) {
-        permutate("",s,0);
+        vector<string> ans;
+        permutate("",s,0, ans);
         return ans;
     }
 };

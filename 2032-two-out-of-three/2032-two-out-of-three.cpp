fb6bbdb136ab140e6a6d6a4960de6c2cd5ca9cdc
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-03 01:52:24 +0530

    Time: 28 ms (53.62%), Space: 32.5 MB (5.61%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        unordered_map<int,unordered_set<int>>st;
+        for(int i=0;i<nums1.size();i++){
+            st[nums1[i]].insert(1);
+        }
+        for(int i=0;i<nums2.size();i++){
+            st[nums2[i]].insert(2);
+        }
+        for(int i=0;i<nums3.size();i++){
+            st[nums3[i]].insert(3);
+        }
+        vector<int>result;
+        for(auto it=st.begin();it!=st.end();it++){
+            if(it->second.size()>=2)
+            {
+                result.push_back(it->first);
+            }
+        }
+        return result;
+    }
+};

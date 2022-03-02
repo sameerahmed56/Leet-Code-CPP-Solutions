fc8504cdfb8a1c08cd99e0ebd84f59c56a2aa266
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-02 14:50:20 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -3,13 +3,7 @@ public:
     bool isSubsequence(string s, string t) {
         int currS = 0, currT = 0;
         while(currT < t.size() && currS <  s.size()){
-            if(s[currS] == t[currT]){
-                currS++;
-                currT++;
-            }
-            else{
-                currT++;
-            }
+            if(s[currS] == t[currT++]) currS++;
         }
         return currS == s.size();
     }

a772cb2a9b577dc5a282166e372cc3abdfad92fd
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-03 02:06:34 +0530

    Time: 45 ms (18.12%), Space: 26.5 MB (65.15%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
-        unordered_map<int,unordered_set<int>>st;
+        unordered_map<int,int>st;
         for(int i=0;i<nums1.size();i++){
-            st[nums1[i]].insert(1);
+            st[nums1[i]] = 1;
         }
         for(int i=0;i<nums2.size();i++){
-            st[nums2[i]].insert(2);
+            if(st[nums2[i]] == 1 || st[nums2[i]] == 2){
+                st[nums2[i]] = 2;
+            } 
+            else{
+                st[nums2[i]] = -1;
+            }
         }
         for(int i=0;i<nums3.size();i++){
-            st[nums3[i]].insert(3);
+            if(st[nums3[i]] == 1 || st[nums3[i]] == -1 ||  st[nums3[i]] == 2){
+                st[nums3[i]] = 2;
+            }
+            else{
+                st[nums3[i]] = 0;        
+            }
         }
         vector<int>result;
         for(auto it=st.begin();it!=st.end();it++){
-            if(it->second.size()>=2)
+            if(it->second>=2)
             {
                 result.push_back(it->first);
             }

9cef513f0e0a36f80dc1cb420d5182a4b7d99e95
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-05 04:22:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (42.99%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isFeasible(vector<int>& w, int days, int maxCapacity){
+        int count = 0, i = 0, currSum = 0;
+        while(i<w.size()){
+            if(w[i] > maxCapacity){
+                return false;
+            }
+            currSum+=w[i];
+            if(currSum<=maxCapacity)i++;
+            else{
+                currSum=0;
+                count++;
+            }
+        }
+        count = currSum ==0?count:count+1;
+        return count<=days;
+    }
+    int splitArray(vector<int>& w, int days) {
+        int sum =accumulate(w.begin(),w.end(),0), l = 0, r=sum, ans = INT_MAX;
+        while(l<=r){
+            int mid = l + (r - l) / 2;
+            if(isFeasible(w,days,mid)){
+                r=mid-1;
+                ans = min(mid,ans);
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};

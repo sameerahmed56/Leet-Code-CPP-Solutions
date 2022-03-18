e4f6ea51e2ae5a86ddb8aac66550f65eca4942eb
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-19 00:10:45 +0530

    Time: 266 ms (22.00%), Space: 63.1 MB (56.40%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,27 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* createTree(vector<int> arr, int l, int r){
+    vector<int> arr;
+    TreeNode* createTree(int l, int r){
         if(l>r) return NULL;
         int mid = l + (r - l) / 2;
         TreeNode* root = new TreeNode(arr[mid]);
-        root->left = createTree(arr,l,mid-1);
-        root->right = createTree(arr,mid+1,r);
+        root->left = createTree(l,mid-1);
+        root->right = createTree(mid+1,r);
         return root;
     }
-    void traverse(TreeNode* root,vector<int> &arr){
+    void traverse(TreeNode* root){
         if(root==NULL) return;
-        traverse(root->left,arr);
+        traverse(root->left);
         arr.push_back(root->val);
-        traverse(root->right,arr);
+        traverse(root->right);
     }
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> arr;
-        traverse(root,arr);
-        int l = 0, r = arr.size()-1, mid = l + (r - l) / 2;
-        TreeNode* ans = new TreeNode(arr[mid]);
-        ans->left = createTree(arr,l,mid-1);
-        ans->right = createTree(arr,mid+1,r);
-        return ans;
+        traverse(root);
+        int l = 0, r = arr.size()-1;
+        root = createTree(l,r);
+        return root;
     }
 };

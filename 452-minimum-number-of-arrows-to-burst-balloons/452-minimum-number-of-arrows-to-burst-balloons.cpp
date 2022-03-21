55d914e16d62c78fddc43049f9af994844fd5000
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-21 15:28:01 +0530

    Time: 657 ms (45.31%), Space: 89.8 MB (28.54%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -4,18 +4,10 @@ public:
         sort(points.begin(),points.end(),[&](vector<int> &a, vector<int>&b){
             return a[1]<b[1];
         });
-        int ans = 1, start = points[0][0], end = points[0][1], n=points.size();
-        bool fired = false;
+        int n=points.size(),ans=n ,end = points[0][1];
         for(int i=1; i<n;i++){
-            ans++;
-            if(end >= points[i][0]){
-                ans--;
-            }
-            else{
-                end = points[i][1];
-                start = points[i][0];
-                fired = false;
-            }
+            if(end >= points[i][0]) ans--;
+            else end = points[i][1];
         }
         return ans;
     }

05d4223b7b4674972e0bdf9cbc30ce64fa2794dc
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-22 17:51:37 +0530

    Time: 23 ms (77.84%), Space: 13.9 MB (55.65%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -9,19 +9,16 @@ public:
             inorder[a[0]]++;
             adj[a[1]].push_back(a[0]);
         }
-        for(int i=0;i<n;i++){
-            if(inorder[i]==0){
-                q.push(i);
-            }
-        }
+        for(int i=0;i<n;i++)
+            if(inorder[i]==0)q.push(i);
+        
         while(!q.empty()){
-            count++;
             int curr = q.front();
             q.pop();
             for(auto i: adj[curr]){
-                inorder[i]--;
-                if(inorder[i]==0) q.push(i);
+                if(--inorder[i]==0) q.push(i);
             }
+            count++;
         }
         return count == n;
     }

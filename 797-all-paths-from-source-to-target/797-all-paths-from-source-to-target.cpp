2f53be7e7f7df27fc13775318866a711e7029013
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-26 04:04:30 +0530

    Time: 48 ms (19.14%), Space: 16.6 MB (26.70%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void getPath(vector<vector<int>>& graph, int pos, int n, vector<int> arr){
+        arr.push_back(pos);
+        if(pos==n-1){
+            ans.push_back(arr);
+            return;
+        }
+        for(int i: graph[pos]){
+            getPath(graph,i,n, arr);
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        getPath(graph,0,n, {});
+        return ans;
+    }
+};

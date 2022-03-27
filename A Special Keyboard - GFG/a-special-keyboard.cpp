7efd2fa3d3ea84bf73e93192cbf07c75c6779e21
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-27 05:54:44 +0530

    Added solution - LeetHub

---
+++ A Special Keyboard - GFG/a-special-keyboard.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int findTime(string S1, string S2) {
+        // code here
+        map<int,int>mp;
+        for(int i=0;i<S1.size();i++)mp[S1[i]] = i;
+        int curr = 0, ans =0;
+        for(char c: S2){
+            ans+= abs(mp[c]-curr);
+            curr = mp[c];
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1,S2;
+        cin>>S1;
+        cin>>S2;
+
+        Solution ob;
+        cout<<ob.findTime(S1,S2)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends

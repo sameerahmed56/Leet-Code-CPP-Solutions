b6808f6527388ee5f5b4c74bdcb7c41ca5a6b4b9
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-28 00:40:00 +0530

    Time: 12 ms (83.53%), Space: 10.3 MB (91.37%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<pair<int,int>> freq;
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> freq;
         int m = mat.size(), n = mat[0].size();
         for(int i=0;i< m; i++){
             int count = 0;
@@ -10,12 +10,12 @@ public:
                     break;
                 count++;
             }
-            freq.push_back({count,i});
+            freq.push({count,i});
         }
-        sort(freq.begin(),freq.end());
         vector<int> ans;
         for(int i=0;i<k;i++){
-            ans.push_back(freq[i].second);
+            ans.push_back(freq.top().second);
+            freq.pop();
         }
         return ans;
     }

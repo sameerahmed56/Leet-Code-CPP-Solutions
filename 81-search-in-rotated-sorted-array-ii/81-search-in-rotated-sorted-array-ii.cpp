a955455ec7f57373a7a35c37f9d94d77ed980faf
Sameer Ahmed <samahmed78656@gmail.com>
2022-03-28 17:57:32 +0530

    Time: 14 ms (23.27%), Space: 14 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -7,14 +7,6 @@ public:
         if(target == mid) return true;
         return binarySearch(nums,target, m + 1, r) || binarySearch(nums,target, l,m-1);
     }
-//     int searchPivot(vector<int>& nums, int l, int r){
-//         if(l>r)return -1;
-//         int m = l+((r-l)/2);
-//         if(nums[mid-1] > nums[mid]){
-//             return mid;
-//         }
-        
-//     }
     bool search(vector<int>& nums, int target) {
         return binarySearch(nums,target, 0 , nums.size()-1);
     }

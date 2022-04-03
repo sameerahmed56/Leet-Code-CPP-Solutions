712915eaf58736b5c51d22fdd1ecffcb6bd39d2e
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-04 02:09:35 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (37.30%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(next_permutation(nums.begin(),nums.end())){
+            return;
+        }
+        else{
+            sort(nums.begin(), nums.end());
+            return;
+        }
+    }
+};

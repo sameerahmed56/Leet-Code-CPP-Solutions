e58f6341e1a6331ca4c9132bfaf83522d5745829
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-05 02:13:45 +0530

    Time: 69 ms (42.82%), Space: 18.8 MB (94.38%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isFeasible(vector<int>& dist, double hour, int speed){
+        double time = 0;
+        for(int i=0;i<dist.size();i++){
+            time +=(double)dist[i]/(double)speed;
+            if(i!=dist.size()-1){
+                time = ceil(time);
+            }
+        }
+        return time<=hour;
+    }
+    int minEatingSpeed(vector<int>& dist, int hour) {
+        int l=1,r=1e9, ans= -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(isFeasible(dist,hour,mid)){
+                ans = mid;
+                r=mid-1;
+            }
+            else{
+                l= mid+1;
+            }
+        }
+        return ans;
+    }
+};

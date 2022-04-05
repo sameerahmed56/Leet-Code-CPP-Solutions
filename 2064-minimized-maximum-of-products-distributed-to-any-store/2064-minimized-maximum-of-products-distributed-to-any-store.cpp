17e3a8bd1c69d439f73e246135df5270a1d4c24e
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-06 01:44:22 +0530

    Time: 336 ms (40.43%), Space: 84.5 MB (47.48%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    int ans = INT_MAX;
     bool isFeasible(vector<int>& nums, int n, int prod){
-        int count = 0, maxValue = 0;
+        int count = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]/prod > 0) maxValue = prod;
-            count += nums[i]/prod;
-            if(nums[i]%prod != 0){
-                count++;
-                maxValue = max(maxValue,nums[i]%prod);
-            }
-        }
-        if(count <= n){
-            ans = min(ans,maxValue);
+            count += (nums[i] + prod - 1) / prod;
         }
         return count<=n ;
     }
     int minimizedMaximum(int n, vector<int>& nums) {
         int l=1,r=1e5;
-        while(l<=r){
+        while(l<r){
             int mid = l + (r-l)/2;
             if(isFeasible(nums,n,mid)){
-                r= mid-1;
+                r= mid;
             }
             else{
                 l= mid+1;
             }
         }
-        return ans;
+        return l;
     }
 };

706d3be4f61a77619d2e48bbdba7616fd102ea61
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-07 21:15:33 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -4,16 +4,9 @@ public:
         priority_queue<int> pq;
         for(int i: stones) pq.push(i);
         while(pq.size()>1){
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
-            if(a>b){
-                pq.push(a-b);
-            }
-            else if(b>a){
-                pq.push(b-a);
-            } 
+            int a = pq.top(); pq.pop();
+            int b = pq.top(); pq.pop();
+            if(a>b) pq.push(a-b);
         }
         return !pq.empty() ?  pq.top():0;
     }

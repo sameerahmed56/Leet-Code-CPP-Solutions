56aaea925dea8104931459105d491036aa9ff236
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-08 15:18:01 +0530

    Time: 55 ms (65.80%), Space: 22.3 MB (58.53%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -12,9 +12,7 @@ public:
         int l=1,r=1e6, ans = 0;
         while(l<=r){
             double mid = l + (r-l)/2;
-            if(isFeasible(nums,threshold,mid)){
-                r=mid-1;
-            }
+            if(isFeasible(nums,threshold,mid)) r=mid-1;
             else l=mid+1;
         }
         return l;

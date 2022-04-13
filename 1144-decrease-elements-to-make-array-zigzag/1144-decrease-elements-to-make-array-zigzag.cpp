6bd226ff57e4ca794a343ec51db5a9514209c048
Sameer Ahmed <samahmed78656@gmail.com>
2022-04-13 15:58:50 +0530

    Time: 4 ms (30.41%), Space: 7.3 MB (53.09%) - LeetHub

--- 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.cpp
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.cpp
@@ -4,20 +4,16 @@ public:
         int n=nums.size(), evenCount = 0, oddCount = 0;
         for(int i=0;i<n;i++){
             int minElement = INT_MAX;
-            if(i%2==0){
-                if(i!=0){
+            if(i!=0){
                     minElement = min(minElement,nums[i-1]);
-                }
-                if(i!=n-1){
-                    minElement = min(minElement,nums[i+1]);
-                } 
+            }
+            if(i!=n-1){
+                minElement = min(minElement,nums[i+1]);
+            } 
+            if(i%2==0){
                 if(nums[i]>=minElement) evenCount+= nums[i]-minElement+1;
             }
             else{
-                minElement = min(minElement,nums[i-1]);
-                if(i!=n-1){
-                    minElement = min(minElement,nums[i+1]);
-                } 
                 if(nums[i]>=minElement) oddCount+= nums[i]-minElement+1;
             }
         }

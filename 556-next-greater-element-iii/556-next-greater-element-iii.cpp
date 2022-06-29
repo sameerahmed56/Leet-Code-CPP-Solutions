e6eb99486f9571ec3452fdc9d14f900fa85e98f8
Sameer Ahmed <samahmed78656@gmail.com>
2022-06-29 22:53:48 +0530

    Time: 3 ms (37.42%), Space: 5.8 MB (93.25%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -1,33 +1,9 @@
 class Solution {
 public:
-    void nextPermutation(string &nums) {
-        int n = nums.size() - 1, prev = nums[n], ind1 = -1, ind2 = -1;
-        for(int i=n-1; i>=0;i--){
-            if(nums[i] < prev){
-                ind1 = i;
-                break;
-            }
-            prev = nums[i];
-        }
-        if(ind1 == -1){
-            nums = "2147483648";
-            return;
-        }
-        for(int i=n;i>=0;i--){
-            if(nums[i] > nums[ind1]){
-                ind2 = i;
-                break;
-            }
-        }
-        swap(nums[ind1],nums[ind2]);
-        reverse(nums.begin() + ind1 + 1, nums.end());
-
-    }
     int nextGreaterElement(int n) {
-        string nums = to_string(n);
-        nextPermutation(nums);
-        long myLong = std::stol( nums );
-        if(myLong>INT_MAX) return -1;
-        return (int)myLong;
+        auto digits = to_string(n);
+        next_permutation(begin(digits), end(digits));
+        auto res = stoll(digits);
+        return (res > INT_MAX || res <= n) ? -1 : res;
     }
 };

7ea9ad74adc301c0d96a8a7da7e6ab5876160061
Sameer Ahmed <samahmed78656@gmail.com>
2022-07-05 00:39:51 +0530

    Time: 27 ms (74.08%), Space: 17.2 MB (96.69%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -6,17 +6,11 @@ public:
         if(n<=1) return n;
         bool isInc = arr[1] > arr[0];
         while(i < n){
-            if(arr[i] > arr[i-1]){
-                !isInc ? prev = 2 : prev++; 
+            if(arr[i] > arr[i-1] || arr[i] == arr[i-1]){
+                arr[i] == arr[i-1] ? prev = 1 : !isInc ? prev = 2 :  prev++; 
                 stack = 1;
                 ans+=prev;
                 isInc = true;
-            }
-            else if(arr[i] == arr[i-1]){
-                prev = 1;
-                ans += prev;
-                isInc = true;
-                stack = 1;
             }
             else{
                 stack++;
@@ -28,8 +22,4 @@ public:
         return ans;
     }
 };
-
-// [1,2,2,2,2,2,3,4,5,6,7,8,7,6,5,4,3,2,1,0,0,1,5,6,8,1]
-// [1,2,1,1,1,1,2,3,4,5,6,7,6,5,4,3,2,1,1,2,3,4,1]
-// [1,3,4,5,6,7,9,12,16,21,27,34,6,5,4,3,2,1,2,3,4,1]
 

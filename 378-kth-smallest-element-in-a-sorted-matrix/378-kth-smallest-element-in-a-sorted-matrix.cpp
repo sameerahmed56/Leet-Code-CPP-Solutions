65291a2bfed19f25ec45bb584e33a6e6b20c7eb8
Sameer Ahmed <samahmed78656@gmail.com>
2022-07-05 03:38:20 +0530

    Time: 77 ms (20.06%), Space: 13.2 MB (60.02%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -8,21 +8,18 @@ public:
     int kthSmallest(vector<vector<int>>& A, int k) {
         int n = A.size(), count = 1;
         priority_queue <tuple <int,int,int>, vector<tuple <int,int,int>>, compare > pq;
-        pq.push(make_tuple(A[0][0],0,0));
-        A[0][0] = INT_MAX;
+        for(int j=0; j<n; j++){
+            pq.push(make_tuple(A[0][j],0,j));
+            A[0][j] = INT_MAX;  
+        }
         while(!pq.empty()){
+            if(count == k) return get<0>(pq.top());
             int x = get<1>(pq.top());
             int y = get<2>(pq.top());
-            if(count == k) return get<0>(pq.top());
             pq.pop();
             if(x<n-1 && A[x+1][y] < INT_MAX){
                 pq.push(make_tuple(A[x+1][y],x+1,y));
                 A[x+1][y] = INT_MAX;
-            }
-            if(y<n-1 && A[x][y+1] < INT_MAX){
-                pq.push(make_tuple(A[x][y+1],x,y+1));
-                A[x][y+1] = INT_MAX;
-                
             }
             count++;
         }

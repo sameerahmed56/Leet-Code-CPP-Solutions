812fd374ba18d2f18e5d3c7f2864346ab185a228
Sameer Ahmed <samahmed78656@gmail.com>
2022-07-05 16:05:58 +0530

    Time: 1002 ms (15.75%), Space: 49.7 MB (52.56%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -10,12 +10,13 @@ public:
         int ans = 0;
         
         for(int i=0;i<nums.size();i++){
-            
+            //minimum element to be searched for the purpose of comparing the next +1 value
             if(s.find(nums[i]-1) == s.end()){
-                int j = nums[i]; 
-                
+                int j = nums[i]; // current value
+                // next values while true do j++
                 while(s.find(j)!=s.end())
                     j++;
+                // max of previous ans and j - nums[i](-> smallest value)
                 ans = max(ans,j-nums[i]);
             }
         }

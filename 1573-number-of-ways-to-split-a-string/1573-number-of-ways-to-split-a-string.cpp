cecc62445451c1e386526936d51eacb09d38b5b7
Sameer Ahmed <samahmed78656@gmail.com>
2022-07-06 16:48:54 +0530

    Time: 61 ms (31.08%), Space: 13.9 MB (67.69%) - LeetHub

---
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numWays(string s) {
+        long long mod = 1000000007;
+        long long oneCount = 0, ans = 1, n = s.size();
+        for(char c: s){
+            if(c=='1') oneCount++;
+        }
+        if(oneCount%3 != 0) return 0;
+        if(oneCount == 0){
+            ans = ((n-1)*(n-2)/2)%mod;
+        }
+        else{
+            oneCount /= 3;
+            bool found = true;
+            long long count = 0, midCount = 0;
+            for(char c: s){
+                if(c=='1'){
+                    count++;
+                    if(midCount != 0){
+                        ans = ans * (midCount+1) % mod;
+                    } 
+                    midCount = 0;
+                }
+                else if(count != 0 && count % oneCount == 0 && count < 3*oneCount){
+                    midCount++;
+                }
+            }   
+        }
+        return (int)ans;
+    }
+};

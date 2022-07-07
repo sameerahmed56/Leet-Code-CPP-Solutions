cc316104ad93919c1b637f9c5545f39083fcc038
Sameer Ahmed <samahmed78656@gmail.com>
2022-07-07 19:17:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s3.length() != s1.length() + s2.length()) return false;
-        
-        bool table[s1.length()+1][s2.length()+1];
-    
+         vector<bool> table(s2.length()+1, false);
         for(int i=0; i<s1.length()+1; i++){
             for(int j=0; j< s2.length()+1; j++){
-                if(i==0 && j==0)
-                    table[i][j] = true;
+                if(i == 0 && j==0)
+                    table[j] = true;
                 else if(i == 0)
-                    table[i][j] = (table[i][j-1] && s2[j-1] == s3[i+j-1]);
+                    table[j] = (table[j-1] && s2[j-1] == s3[i+j-1]);
                 else if(j == 0)
-                    table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+                    table[j] = ( table[j] && s1[i-1] == s3[i+j-1]);
                 else
-                    table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+                    table[j] = (table[j] && s1[i-1] == s3[i+j-1] ) || (table[j-1] && s2[j-1] == s3[i+j-1] );
             }
         }
-        return table[s1.size()][s2.size()];
+        return table[s2.size()];
     }        
 };
+// false false false false 

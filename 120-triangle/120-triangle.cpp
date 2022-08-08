9b24c6160b888635e26373c5695012ef8c741c88
Sameer Ahmed <samahmed78656@gmail.com>
2022-08-08 15:23:31 +0530

    Time: 0 ms (100.00%), Space: 9 MB (23.26%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int findMinPath(vector<vector<int>>& triangle, int i,int j, vector<vector<int>>& dp){
-        int n = triangle.size();
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j] != -1)  return dp[i][j];
-        
-        int down = triangle[i][j] + findMinPath(triangle,i+1,j, dp);
-        int diag = triangle[i][j] + findMinPath(triangle,i+1,j+1, dp);
-        
-        dp[i][j] = min(down,diag);
-        return dp[i][j];
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
+        if(n==1) return triangle[0][0];
         vector<vector<int>> dp(n);
         for(int i=0; i<n; i++){
             vector<int> tmp(i+1,-1);
             dp[i] = tmp;
         }
-        return findMinPath(triangle,0,0, dp);
+        int ans=INT_MAX;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                
+                int up = 1e9;
+                int diag = 1e9;
+                if(i==0 && j == 0){
+                    dp[i][j] = triangle[i][j];
+                    continue;
+                }
+                if(i>0 && j <i) up = triangle[i][j] + dp[i-1][j];
+                if(j>0 && i>0) diag = dp[i-1][j-1] + triangle[i][j];
+                
+                dp[i][j] = min(up,diag);
+                
+                if(i==n-1){
+                    ans = min(dp[i][j],ans);
+                }
+            }
+        }
+
+        return ans;
     }
 };

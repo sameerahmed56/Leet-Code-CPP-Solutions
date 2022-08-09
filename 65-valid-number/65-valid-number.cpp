fa91328990c940e7704b4a0dce76c6f104a733da
Sameer Ahmed <samahmed78656@gmail.com>
2022-08-09 17:31:33 +0530

    Time: 8 ms (24.16%), Space: 5.9 MB (93.77%) - LeetHub

---
+++ 65-valid-number/65-valid-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool sign = 0, decimal = 0, digit = 0, expo = 0;
+        int n=s.size();
+        if(n==0) return false;
+        for(char c: s){ 
+            if(c == '+' || c == '-'){
+                if(digit || sign) return false;
+                if(decimal && !expo) return false;
+                sign = 1;
+            }
+            else if(c >= '0' && c<= '9'){
+                digit = 1;
+            }
+            else if(c == '.'){
+                if(decimal || expo) return false;
+                decimal = 1;
+            }
+            else if(c == 'e' || c == 'E'){
+                if(!digit || expo) return false;
+                sign = 0;
+                digit = 0;
+                expo = 1;
+            }
+            else return false;
+        }
+        return digit;
+    }
+};

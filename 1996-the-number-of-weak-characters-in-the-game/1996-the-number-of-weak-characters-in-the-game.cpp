325bfb38481850a4ab1eec133eb18c989649eeea
Sameer Ahmed <samahmed78656@gmail.com>
2022-09-09 23:12:52 +0530

    Time: 1576 ms (8.58%), Space: 126.5 MB (37.54%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -5,24 +5,16 @@ public:
         return v1[0] == v2[0] ? v1[1] > v2[1] : v1[0] < v2[0];
     }
     int numberOfWeakCharacters(vector<vector<int>>& prop) {
-        int n = prop.size();
+        int n = prop.size(), ans = 0;
         if(n==0) return 0;
         sort(prop.begin(),prop.end(), sortcol);
-        int ans = 0;
         stack<int> st;
-        st.push(prop[0][1]);
-        for(int i=1;i <n; i++){
-            // cout << prop[i][0] << "-"<< prop[i][1] << " ";             
-            while( !st.empty() &&  st.top() < prop[i][1] ){
-                // cout << st.top()  << "-" <<  prop[i][1] << " ";
+        for(int i=0;i <n; i++){
+            while(!st.empty() &&  st.top() < prop[i][1] ){
                 st.pop();
                 ans++;
             }
             st.push(prop[i][1]);
-            // while( !q.empty()){
-            //     st.push(q.front());
-            //     q.pop();
-            // }
         }
         return ans;
     }

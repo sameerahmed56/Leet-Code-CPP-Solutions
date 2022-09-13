62e4681db57f471b8829b9a3d08a4cc87af11e7c
Sameer Ahmed <samahmed78656@gmail.com>
2022-09-13 21:17:33 +0530

    Time: 26 ms (31.17%), Space: 14.1 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public: 
+    bool nextKBytes(int &i,vector<int> &data,int k,int n)
+    {
+        if(i >= n-k)return false;
+        int j = 0;
+        while(j < k)
+        {   
+            i++;
+            if(data[i] < 128||data[i] >= 192)return false;
+            j++;
+        }
+        return true;
+    }
+    bool validUtf8(vector<int>& data) {
+       int n = data.size();
+        for(int i = 0;i < n;i++)
+        {
+            if(data[i]>191)
+            {   
+                if(data[i] > 247)return false;
+                if(data[i] > 239)
+                {   
+                    if(!nextKBytes(i,data,3,n))return false;
+                }
+                else if(data[i] > 223)
+                {
+                    if(!nextKBytes(i,data,2,n))return false;
+                }
+                else if(data[i] > 191)
+                {
+                    if(!nextKBytes(i,data,1,n))return false;
+                }
+                else false;
+
+            }
+            else if(data[i] >= 128 && data[i] < 192)return false;
+        }
+        
+        return true;    
+    }
+};

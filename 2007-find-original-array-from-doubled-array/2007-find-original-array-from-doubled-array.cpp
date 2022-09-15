eb93b9a914b36f66affa1a687254d0318cc42043
Sameer Ahmed <samahmed78656@gmail.com>
2022-09-16 01:40:04 +0530

    Time: 640 ms (63.59%), Space: 116.7 MB (97.89%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& A) {
         int n = A.size();
-        if(n%2!=0 || n == 0) return {};
+        if(n%2!=0 ) return {};
         vector<int> ans;
         sort(A.begin(),A.end());
         int l = 0, r = 1;
         while(l<n && r < n){
             if(A[l] < 0) l++;
-            else if(A[r] < 0 || l>=r) r++;
+            else if(A[r] < 0 || l >=r) r++;
             else if(A[l]*2 == A[r]){
                 ans.push_back(A[l]);
-                A[r] = -1; 
-                A[l] = -1;
-                l++;
-                r++;
+                A[r++] = -1; 
+                A[l++] = -1;
             }
             else r++;
         }
